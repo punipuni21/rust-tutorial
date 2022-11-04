e69d6e1e4836120eff94e2ce7c752812d60c1d1e
punipuni21 <punihobby@gmail.com>
2022-11-04 12:33:56 +0900

    type result

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    // Resultについて
+    let result: Result<i32, String> = Ok(200);
+
+    match result {
+        Ok(code) => println!("code: {}", code),
+        Err(err) => println!("Err: {}", err),
+    }
+
+    // unwqp_or_elseも使用できる
+    let result2: Result<i32, String> = Ok(200);
+    println!("code2: {}", result2.unwrap_or(-1))
 }
