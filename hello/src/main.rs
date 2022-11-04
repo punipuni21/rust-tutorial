408c78a09b7cdbfd46c936391ff3b01c43ac4556
punipuni21 <punihobby@gmail.com>
2022-11-04 12:48:37 +0900

    モジュールに分ける

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,13 +1,4 @@
+mod result_sample;
 fn main() {
-    // Resultについて
-    let result: Result<i32, String> = Ok(200);
-
-    match result {
-        Ok(code) => println!("code: {}", code),
-        Err(err) => println!("Err: {}", err),
-    }
-
-    // unwqp_or_elseも使用できる
-    let result2: Result<i32, String> = Ok(200);
-    println!("code2: {}", result2.unwrap_or(-1))
+    result_sample::result_sample()
 }
---
+++ hello/src/result_sample.rs
@@ -0,0 +1,13 @@
+pub fn result_sample() -> () {
+    // Resultについて
+    let result: Result<i32, String> = Ok(200);
+
+    match result {
+        Ok(code) => println!("code: {}", code),
+        Err(err) => println!("Err: {}", err),
+    }
+
+    // unwqp_or_elseも使用できる
+    let result2: Result<i32, String> = Ok(200);
+    println!("code2: {}", result2.unwrap_or(-1))
+}
