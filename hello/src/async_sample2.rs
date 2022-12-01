9a4de4ea99d528ebf4ba19ad95d75dcb6b49cfb0
punipuni21 <watanaberyotaroyp@gmail.com>
2022-12-01 22:36:15 +0900

    別の実装

--- hello/src/async_sample2.rs
+++ hello/src/async_sample2.rs
@@ -1,9 +1,14 @@
+use futures::future::Future;
+use std::process::Output;
+
 async fn async_add(left: i32, right: i32) -> i32 {
     left + right
 }
 
-pub async fn something_greet_async_function() -> i32 {
-    let ans = async_add(2, 3).await;
-    println!("{}", ans);
-    ans
+pub fn something_greet_async_function() -> impl Future<Output = i32> {
+    async {
+        let ans = async_add(2, 3).await;
+        println!("{}", ans);
+        ans
+    }
 }
